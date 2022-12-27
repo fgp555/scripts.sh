e99acaf88f55956ebd0e7b40c886b66e9fdb277b
Frank GP <fgp555@gmail.com>
2022-12-27 11:19:29 -0500

    01 Commit inicial

---
+++ git status && --oneline.sh
@@ -0,0 +1,13 @@
+echo ""
+echo "==================="
+echo "git status..."
+echo "==================="
+git status
+echo ""
+echo "==================="
+echo "git log --oneline..."
+echo "==================="
+git log --oneline
+echo ""
+
+read -t 30 -p "Press [Enter] key to exit..."
